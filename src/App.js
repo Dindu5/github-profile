6384e566845d5eb33aafde8f0a6511addeaddcae
Dindu5 <chimdindue@gmail.com>
2020-11-23 13:41:35 +0100

    added media queries and responsive layout

--- src/App.js
+++ src/App.js
@@ -1,48 +1,27 @@
 import React from "react";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import NavBar from "./components/NavBar";
 import TextField from "@material-ui/core/TextField";
-
-import PersonIcon from "@material-ui/icons/Person";
-import GroupAddIcon from "@material-ui/icons/GroupAdd";
-import BusinessCenterIcon from "@material-ui/icons/BusinessCenter";
 import Skeleton from "@material-ui/lab/Skeleton";
 import axios from "axios";
 
-import { makeStyles } from "@material-ui/core/styles";
-import Table from "@material-ui/core/Table";
-import TableBody from "@material-ui/core/TableBody";
-import TablePagination from "@material-ui/core/TablePagination";
-import TableCell from "@material-ui/core/TableCell";
-import TableContainer from "@material-ui/core/TableContainer";
-import TableHead from "@material-ui/core/TableHead";
-import TableRow from "@material-ui/core/TableRow";
-import Paper from "@material-ui/core/Paper";
-import Timestamp from "react-timestamp";
-import Button from "@material-ui/core/Button";
-
-const useStyles = makeStyles({
-  table: {
-    minWidth: 650,
-  },
-});
+import NewTable from "./components/NewTable";
+import Details from "./components/Details";
+import Main from "./components/Main";
+import CancelIcon from "@material-ui/icons/Cancel";
 
 export default function App() {
-  const classes = useStyles();
-
   const [search, setsearch] = useState("example");
   const [userSearch, setUserSearch] = useState("");
   const [userdata, setUserData] = useState({});
   const [repositores, setRepositores] = useState({});
   const [loading, setLoading] = useState(true);
-  const [page, setPage] = React.useState(0);
-
-  const [rowsPerPage, setRowsPerPage] = React.useState(5);
+  const [error, setError] = useState("");
+  const message = useRef();
 
   useEffect(() => {
     const url1 = `https://api.github.com/users/${search}`;
     const url2 = `https://api.github.com/users/${search}/repos`;
-
     const userDetails = axios.get(url1);
     const repoDetails = axios.get(url2);
 
@@ -59,27 +38,19 @@ export default function App() {
         })
       )
       .catch((err) => {
-        console.log("there was an error", err.response);
+        console.log("there was an error", err.message);
+        const messageBox = message.current;
+
+        if (err.message === "Network Error") {
+          setError("Oops... Seems Your Internet Disconnected");
+        } else {
+          setError("There was an error.. sem");
+        }
+        messageBox.classList.add("open");
       });
   }, [search]);
 
   const repos = Array.from(repositores);
-  console.log("repoarray =", repos);
-
-  const {
-    login,
-    followers,
-    public_repos,
-    avatar_url,
-    bio,
-    following,
-    name,
-    location,
-    html_url,
-    twitter_username,
-    created_at,
-    updated_at,
-  } = userdata;
 
   const captureInput = (e) => {
     setUserSearch(e.target.value);
@@ -90,16 +61,13 @@ export default function App() {
     setLoading(true);
     if (userSearch !== "") {
       setsearch(userSearch);
+      setUserSearch("");
     }
   };
 
-  const handleChangePage = (event, newPage) => {
-    setPage(newPage);
-  };
-
-  const handleChangeRowsPerPage = (event) => {
-    setRowsPerPage(parseInt(event.target.value, 10));
-    setPage(0);
+  const closeError = (e) => {
+    const parent = e.currentTarget.parentElement;
+    parent.classList.remove("open");
   };
 
   return (
@@ -121,107 +89,33 @@ export default function App() {
       </form>
 
       <div className="grid">
-        <div id="main-profile">
-          <div className="img-container">
-            <img src={avatar_url} alt="GitHub User" />
+        {/* Main Details */}
+        s
+        <Main loading={loading} userdata={userdata} />
+        <div className="others">
+          {/* User Details */}
+
+          <div id="content1">
+            <Details userdata={userdata} loading={loading} />
           </div>
 
-          <div className="meta">
-            <h2>{login}</h2>
-          </div>
-
-          <div className="details">
-            <span>
-              <BusinessCenterIcon />
-              <p> {public_repos} Repo(s)</p>
-            </span>
-            <span>
-              <GroupAddIcon />
-              <p>{followers} Followers</p>
-            </span>
-            <span>
-              <PersonIcon />
-              <p>{following} Following</p>
-            </span>
-          </div>
-        </div>
-
-        <div id="content1">
-          <div className="items">
-            <span>
-              <p className="heading">Name :</p>
-              <p>{name}</p>
-            </span>
-            <span>
-              <p className="heading">Bio :</p>
-              <p>{bio}</p>
-            </span>
-            <span>
-              <p className="heading">Location :</p>
-              <p>{location}</p>
-            </span>
+          {/* Repositories */}
+          <div id="content4">
             {!loading ? (
-              <span>
-                <p className="heading">Repo Link :</p>
-                <a href={html_url}>{html_url}</a>
-              </span>
+              <NewTable repos={repos} />
             ) : (
-              <Skeleton variant="text" height={10} />
+              <Skeleton variant="rect" height="20vh" width="100%" />
             )}
           </div>
         </div>
-
-        <div id="content4">
-          <TableContainer component={Paper}>
-            <Table
-              className={classes.table}
-              size="small"
-              aria-label="a dense table"
-            >
-              <TableHead>
-                <TableRow>
-                  <TableCell>Repository</TableCell>
-                  <TableCell>Link</TableCell>
-                  <TableCell>Date Created</TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {repos
-                  .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-                  .map((repo) => (
-                    <TableRow key={repo.name}>
-                      <TableCell component="th" scope="repo">
-                        {repo.name}
-                      </TableCell>
-                      <TableCell>
-                        <a href={repo.html_url} target="_blank">
-                          <Button
-                            variant="contained"
-                            size="small"
-                            color="primary"
-                          >
-                            Visit
-                          </Button>
-                        </a>
-                      </TableCell>
-                      <TableCell>
-                        <Timestamp date={repo.created_at} />
-                      </TableCell>
-                    </TableRow>
-                  ))}
-              </TableBody>
-            </Table>
-          </TableContainer>
-          <TablePagination
-            rowsPerPageOptions={[5, 10]}
-            component="div"
-            count={repos.length}
-            rowsPerPage={rowsPerPage}
-            page={page}
-            onChangePage={handleChangePage}
-            onChangeRowsPerPage={handleChangeRowsPerPage}
-          />
-        </div>
+      </div>
+      <div className="error-message" ref={message}>
+        <p className="error">{error}</p>
+        <CancelIcon
+          onClick={(e) => {
+            closeError(e);
+          }}
+        />
       </div>
     </div>
   );
